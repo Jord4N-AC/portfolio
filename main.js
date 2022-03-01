d2d0fccfe9599b8cb1028185736883624d44e354
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-02-28 20:56:06 -0500

    Add functionality to menu icon to open menu when click

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 /* Mobile Menu */
+const body = document.querySelector('body');
+const menuIcon = document.querySelector('.fa-bars');
 const mobileMenu = document.querySelector('#menu-mobile');
 const closeIcon = document.querySelector('#closebtn');
 const menuLinks = document.querySelectorAll('.menu-link-mb');
 
 
-function disapear() {
-  mobileMenu.style.width = '0%';
-  mobileMenu.style.display = 'none';
+function display() {
+  mobileMenu.style.width = '100%';
+  mobileMenu.style.transform = 'translate(0%)';
+  body.style.overflow = 'hidden';
 }
 
+function disapear() {
+  mobileMenu.style.width = '0%';
+  mobileMenu.style.transform = 'translate(-100%)';
+  body.style.overflow = 'initial';
+}
+
+menuIcon.addEventListener('click', display);
 closeIcon.addEventListener('click', disapear);
 menuLinks.forEach(link => {
   link.addEventListener('click', disapear);
 });
 
-// console.log(menuLinks);
+console.log(menuIcon);
