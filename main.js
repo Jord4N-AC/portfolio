2e318bacfa12f22d470c7831f13744e6695e65b5
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-03-01 22:06:58 -0500

    Create Section 2 with javaScript, Using the data in the object

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ const cardsContent = {
       'Multi-Post Stories',
       'Profesional Art Printing Data More',
       'Data Dashboard Healthcare',
-      'Website Protfolio '
+      'Website Protfolio'
     ],
     popup_mobile: 'Multi Post Stories',
     popup_desktop: 'Keeping track of hundreds  of components website'
@@ -68,10 +68,70 @@ const cardsContent = {
     popup_desktop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.'
   },
   tags: {
-    card_1: ['css', 'html', 'bootstrap', 'Ruby'],
-    card_2_7: ['css', 'html', 'bootstrap']
+    cards: ['css', 'html', 'bootstrap', 'Ruby']
   },
   buttons: [
     'See Project', 'See Live', 'See Source'
   ]
 };
+
+function createCard() {
+  const worksSection = document.querySelector('#works-section');
+  const header = `
+        <header id="works-header">
+            <h2>My Recent Works</h2>
+            <div id="header-line"></div>
+        </header>
+  `;  
+
+  for(let i = 7; i >= 1; i -= 1) {
+    if(i === 1) {
+      const card = `
+        <div id="main-card" class="card${i}">
+            <img src="${cardsContent.images.card_1}" alt="project${i} screenshot">
+            <div class="content">
+                <h3>${cardsContent.header.mobile[0]}</h3>
+                <p id="card${i}-p" class="p-format">
+                    ${cardsContent.paragraph.card_1}
+                </p>
+                <ul class="tag-group">
+                    <li class="tag">${cardsContent.tags.cards[0]}</li>
+                    <li class="tag">${cardsContent.tags.cards[1]}</li>
+                    <li class="tag">${cardsContent.tags.cards[2]}</li>
+                    <li class="tag">${cardsContent.tags.cards[3]}</li>
+                </ul>
+                <a class="btn see-project-btn main-btn" href="#">
+                    ${cardsContent.buttons[0]}
+                </a>
+            </div>
+        </div>
+      `;
+      worksSection.insertAdjacentHTML('afterbegin', card);
+      break;
+    } 
+    
+    const card = `
+    <div class="card${i} card">
+        <div class="content">
+            <h3>${cardsContent.header.mobile[1]}</h3>
+            <p class="p-format">
+                ${cardsContent.paragraph.card_2_7}
+            </p>
+            <ul class="tag-group">
+                <li class="tag">${cardsContent.tags.cards[0]}</li>
+                <li class="tag">${cardsContent.tags.cards[1]}</li>
+                <li class="tag">${cardsContent.tags.cards[2]}</li>
+            </ul>
+        </div>
+        <a class="btn btn-2 see-project-btn" href="#">
+            ${cardsContent.buttons[0]}
+        </a>
+    </div>
+    `;
+    worksSection.insertAdjacentHTML('afterbegin', card);
+  }
+
+  worksSection.insertAdjacentHTML('afterbegin', header);
+}
+
+createCard();
